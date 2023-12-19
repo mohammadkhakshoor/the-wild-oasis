dd8002dd75450023e7daeb8ffe1ca42b7f20591c
mohammad Khakshoor <mkhbeats@gmail.com>
2023-12-19 21:40:29 +0330

    create: add uploading image functionality to the database by user

--- src/ui/Main.jsx
+++ src/ui/Main.jsx
@@ -4,6 +4,7 @@ const StyledMain = styled.main`
   background-color: yellow;
   padding: 4rem 4.8rem 6.4rem;
   background-color: var(--color-grey-50);
+  overflow: scroll;
 `;
 const Main = ({ children }) => {
   return <StyledMain>{children}</StyledMain>;
